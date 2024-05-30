e35baca37d7624ea71012911839d00488354491a
alexquar <alexanderquarrie@gmail.com>
2024-05-30 13:12:52 -0400

    post reqs

--- src/main/java/com/aquarrie/photoz/PhotozController.java
+++ src/main/java/com/aquarrie/photoz/PhotozController.java
@@ -3,11 +3,14 @@ package com.aquarrie.photoz;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -40,5 +43,11 @@ public class PhotozController {
             if (current == null) {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
             }
+        }
+     @PostMapping("/photoz/")
+        public Photo create(@RequestBody Photo photo) {
+           photo.setId(UUID.randomUUID().toString());
+           db.put(photo.getId(), photo);
+           return photo;
         }
 }
