73e161e0a9bdcd70c40e12e437ebe12091744d23
alexquar <alexanderquarrie@gmail.com>
2024-05-30 22:28:12 -0400

    patch reqs and form validation

--- src/main/java/com/aquarrie/photoz/Photo.java
+++ src/main/java/com/aquarrie/photoz/Photo.java
@@ -1,7 +1,10 @@
 package com.aquarrie.photoz;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Photo {
     private String id;
+    @NotEmpty
     private String fileName;
     public Photo(String id, String fileName) {
         this.id = id;
--- src/main/java/com/aquarrie/photoz/PhotozController.java
+++ src/main/java/com/aquarrie/photoz/PhotozController.java
@@ -8,11 +8,14 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class PhotozController {
@@ -45,9 +48,18 @@ public class PhotozController {
             }
         }
      @PostMapping("/photoz/")
-        public Photo create(@RequestBody Photo photo) {
+        public Photo create(@RequestBody @Valid Photo photo) {
            photo.setId(UUID.randomUUID().toString());
            db.put(photo.getId(), photo);
            return photo;
+        }
+    @PatchMapping("/photoz/{id}")
+        public Photo update(@PathVariable String id, @RequestBody @Valid Photo photo) {
+            Photo current = db.get(id);
+            if (current == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
+            current.setFileName(photo.getFileName());
+            return current;
         }
 }
